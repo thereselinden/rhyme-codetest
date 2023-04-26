7f8ec40a5833b42b1dad847f33761af051ddfc7a
Therese Linden <theresemlinden@gmail.com>
2023-04-26 11:05:27 +0200

    delete function and start of edit

--- src/pages/ArticleDetails.jsx
+++ src/pages/ArticleDetails.jsx
@@ -1,23 +1,41 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import SingleArticle from '../components/SingleArticle';
 import useFetch from '../hooks/useFetch';
+import { useState } from 'react';
 
 const ArticleDetails = () => {
+  const [edit, setEdit] = useState(false);
   const { id } = useParams();
+  const navigate = useNavigate();
 
-  const {
-    data: article,
-    error,
-    isLoading,
-  } = useFetch(
-    `http://ec2-3-249-202-253.eu-west-1.compute.amazonaws.com/articles/${id}`
-  );
+  let url = `http://ec2-3-249-202-253.eu-west-1.compute.amazonaws.com/articles/${id}`;
+
+  const { data: article, error, isLoading } = useFetch(url);
+
+  const handleDelete = () => {
+    fetch(url, {
+      method: 'DELETE',
+    }).then(() => {
+      navigate('/');
+    });
+  };
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
 
   return (
     <>
       {error && { error }}
       {isLoading && <div>Loading....</div>}
-      {article && <SingleArticle article={article} />}
+      {article && (
+        <SingleArticle
+          article={article}
+          onDeleteClick={handleDelete}
+          onEditClick={handleEdit}
+          edit={edit}
+        />
+      )}
     </>
   );
 };
