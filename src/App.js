7c3df2a6b4550445c1238d7fe57cd9fb058e3e4c
Therese Linden <theresemlinden@gmail.com>
2023-04-26 08:54:18 +0200

    added wrapping Routes and specified three route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import NavBar from './components/NavBar';
 import FrontPage from './pages/FrontPage';
+import CreateArticle from './pages/CreateArticle';
+import ArticleDetails from './pages/ArticleDetails';
 
 function App() {
   return (
     <div className="App">
-      <FrontPage />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/create" element={<CreateArticle />} />
+        <Route path="/articles/:id" element={<ArticleDetails />} />
+      </Routes>
     </div>
   );
 }
