fd624b817a05800372a8089446a8b9bb0322fa21
Therese Linden <theresemlinden@gmail.com>
2023-04-26 09:41:46 +0200

    fetch single article data

--- src/pages/ArticleDetails.jsx
+++ src/pages/ArticleDetails.jsx
@@ -1,5 +1,25 @@
+import { useParams } from 'react-router-dom';
+import SingleArticle from '../components/SingleArticle';
+import useFetch from '../hooks/useFetch';
+
 const ArticleDetails = () => {
-  return <h2>Hej från ArticleDetails </h2>;
+  const { id } = useParams();
+
+  const {
+    data: article,
+    error,
+    isLoading,
+  } = useFetch(
+    `http://ec2-3-249-202-253.eu-west-1.compute.amazonaws.com/articles/${id}`
+  );
+
+  return (
+    <>
+      {error && { error }}
+      {isLoading && <div>Loading....</div>}
+      {article && <SingleArticle article={article} />}
+    </>
+  );
 };
 
 export default ArticleDetails;
