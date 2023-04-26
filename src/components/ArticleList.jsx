11a3288ad1cb10abc8d8562f2d632c790966c71f
Therese Linden <theresemlinden@gmail.com>
2023-04-26 08:55:30 +0200

    wrapped article preview with Link to redirect to detailpage

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const ArticleList = ({ articles }) => {
   return (
-    <div className="article-preview" key={articles.id}>
+    <div className="article-preview">
       {articles.map(article => (
         <>
-          <h2>{article.title}</h2> <p>Author: {article.author}</p>{' '}
-          <p>Description: {article.description}</p>
+          <div className="article-preview-card" key={article._id}>
+            <Link to={`/articles/${article._id}`}>
+              <h2>{article.title}</h2> <p>Author: {article.author}</p>
+              <p>Description: {article.description}</p>
+            </Link>
+          </div>
         </>
       ))}
     </div>
